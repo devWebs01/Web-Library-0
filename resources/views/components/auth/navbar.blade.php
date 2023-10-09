335634f552be5dacce0a734b7c44b66d6aae52e8
Adi Putra <m.a.syaputra.94@gmail.com>
2023-10-09 16:32:22 +0700

    add feature auth

---
+++ resources/views/components/auth/navbar.blade.php
@@ -0,0 +1,51 @@
+<ul class="menu-inner py-1">
+    <li class="menu-item active">
+        <a href="/home" target="_blank" class="menu-link">
+            <i class="menu-icon tf-icons mdi mdi-email-outline"></i>
+            <div data-i18n="home">Home</div>
+            <div class="badge bg-label-primary fs-tiny rounded-pill ms-auto">Pro</div>
+        </a>
+    </li>
+    <li class="menu-item ">
+        <a href="#" class="menu-link menu-toggle">
+            <i class="menu-icon tf-icons mdi mdi-home-outline"></i>
+            <div data-i18n="Dashboards">Dashboards</div>
+            <div class="badge bg-danger rounded-pill ms-auto">5</div>
+        </a>
+        <ul class="menu-sub">
+            <li class="menu-item">
+                <a href="https://demos.themeselection.com/materio-bootstrap-html-admin-template/html/vertical-menu-template/dashboards-crm.html"
+                    target="_blank" class="menu-link">
+                    <div data-i18n="CRM">CRM</div>
+                    <div class="badge bg-label-primary fs-tiny rounded-pill ms-auto">Pro</div>
+                </a>
+            </li>
+            <li class="menu-item active">
+                <a href="index.html" class="menu-link">
+                    <div data-i18n="Analytics">Analytics</div>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="https://demos.themeselection.com/materio-bootstrap-html-admin-template/html/vertical-menu-template/app-ecommerce-dashboard.html"
+                    target="_blank" class="menu-link">
+                    <div data-i18n="eCommerce">eCommerce</div>
+                    <div class="badge bg-label-primary fs-tiny rounded-pill ms-auto">Pro</div>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="https://demos.themeselection.com/materio-bootstrap-html-admin-template/html/vertical-menu-template/app-logistics-dashboard.html"
+                    target="_blank" class="menu-link">
+                    <div data-i18n="Logistics">Logistics</div>
+                    <div class="badge bg-label-primary fs-tiny rounded-pill ms-auto">Pro</div>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="https://demos.themeselection.com/materio-bootstrap-html-admin-template/html/vertical-menu-template/app-academy-dashboard.html"
+                    target="_blank" class="menu-link">
+                    <div data-i18n="Academy">Academy</div>
+                    <div class="badge bg-label-primary fs-tiny rounded-pill ms-auto">Pro</div>
+                </a>
+            </li>
+        </ul>
+    </li>
+</ul>
