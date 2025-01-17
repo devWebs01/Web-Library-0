0bc7c8c388ed13aa8a2ec607f916a1a69ad11049
Adi Putra <m.a.syaputra.94@gmail.com>
2025-01-17 21:36:45 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,10 @@
 <x-guest.layout>
-    <section class="py-2">
+    {{-- <section class="py-2">
         <div class="container col-xxl-8 px-4 py-5">
             <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
                 <div class="col-10 col-sm-8 col-lg-6">
                     <img src="https://cdn.livecanvas.com/media/svg/pixeltrue/blogging.svg"
-                        class="d-block mx-lg-auto img-fluid wp-image-1461" alt="" loading="lazy" width="640"
-                        height="">
+                        class="d-block mx-lg-auto img-fluid wp-image-1461" alt="" loading="lazy" width="640" height="">
                 </div>
                 <div class="col-lg-6">
                     <div class="lc-block mb-3">
@@ -23,7 +22,21 @@
                 </div>
             </div>
         </div>
+    </section> --}}
+
+    <section class="bg-light text-center py-5">
+        <div class="container pb-5 pt-5">
+            <div class="pb-5 pt-5 row">
+                <div class="col-lg-9 col-xl-8 ms-auto me-auto pb-5 pt-5">
+                    <span>Loreum ipsum</span>
+                    <h1 class="display-3 fw-bold mb-3">Make Good Choices</h1>
+                    <p class="lead mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit tellus
+                        eu.</p><a class="btn btn-lg btn-primary" href="">Learn more</a>
+                </div>
+            </div>
+        </div>
     </section>
+
     <section class="py-4" id="discover">
         <div class="container py-lg-7">
             <h1 class="fw-bold text-center text-primary">Sistem Informasi Perpustakaan <br> Dirancang
